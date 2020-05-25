4eb177ca69c692010e027cd27afdf13da38d4f9f
Walison Filipe <walisonfilipe@hotmail.com>
2020-05-25 02:32:11 -0300

    Replace ugettext with gettext

--- firebase_auth/core/authentication.py
+++ firebase_auth/core/authentication.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.utils.encoding import smart_text
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from rest_framework import exceptions
 from rest_framework.authentication import (
     BaseAuthentication, get_authorization_header
